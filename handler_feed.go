5253719e26ee240c8ceeddbdcedddb9f90748d65
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-14 14:07:28 -0400

    Add feed creation handler
    - Stored with a reference to the user that created it

--- handler_feed.go
+++ handler_feed.go
@@ -3,8 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/MoXcz/gator/internal/database"
+	"github.com/google/uuid"
 )
 
+// Fetch RSS feed
 func handlerAgg(s *state, cmd command) error {
 	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
 	if err != nil {
@@ -12,5 +17,41 @@ func handlerAgg(s *state, cmd command) error {
 	}
 
 	fmt.Printf("Feed: %+v", feed)
+	return nil
+}
+
+// Add RSS feed
+func handlerAddFeed(s *state, cmd command) error {
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUsername)
+	if err != nil {
+		return fmt.Errorf("Error getting user: %w", err)
+	}
+
+	if len(cmd.Args) != 2 {
+		return fmt.Errorf("usage: %s <name> <url>", cmd.Name)
+	}
+	feedName := cmd.Args[0]
+	feedURL := cmd.Args[1]
+
+	feedParams := database.AddFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      feedName,
+		Url:       feedURL,
+		UserID:    user.ID,
+	}
+	feed, err := s.db.AddFeed(context.Background(), feedParams)
+	if err != nil {
+		return fmt.Errorf("Error: Could not add feed, %w", err)
+	}
+
+	fmt.Println("Added record: ")
+	fmt.Println("ID: ", feed.ID)
+	fmt.Println("Name: ", feed.Name)
+	fmt.Println("URL: ", feed.Url)
+	fmt.Println("Created: ", feed.CreatedAt)
+	fmt.Println("On User: ", user.Name)
+
 	return nil
 }
