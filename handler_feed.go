e090a8c30e08bd7b9e61c54b215ff54acfcbf787
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-19 11:23:46 -0400

    Add browse handler
    - Add new table for individual posts
    - Print limited amount of posts for current user

--- handler_feed.go
+++ handler_feed.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/MoXcz/gator/internal/database"
@@ -125,9 +127,66 @@ func scrapeFeed(s *state) error {
 		return err
 	}
 
+	// fmt.Printf("Feed %s with %d posts found:\n", feeds.Channel.Title, len(feeds.Channel.Item))
+	// for i, feed := range feeds.Channel.Item {
+	// 	fmt.Printf("Feed %d: %s\n", i, feed.Title)
+	// }
+
 	fmt.Printf("Feed %s with %d posts found:\n", feeds.Channel.Title, len(feeds.Channel.Item))
-	for i, feed := range feeds.Channel.Item {
-		fmt.Printf("Feed %d: %s\n", i, feed.Title)
+	for _, feed := range feeds.Channel.Item {
+		var desc sql.NullString
+		var pubDate sql.NullTime
+		desc.String = feed.Description
+		pubDate.Time, err = time.Parse(time.RFC1123, feed.PubDate)
+		if err != nil {
+			return err
+		}
+		post, err := s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+			Title:       feed.Title,
+			Url:         feed.Link,
+			Description: desc,
+			PublishedAt: pubDate,
+			FeedID:      lastFeed.ID,
+		})
+		if err != nil {
+			return fmt.Errorf("Error: Could not add post, %w", err)
+		}
+		fmt.Printf("Post %s added\n", post.Title)
+	}
+
+	return nil
+}
+
+func handleBrowse(s *state, cmd command, user database.User) error {
+	var limit int32 = 2
+	if len(cmd.Args) == 1 {
+		l, err := strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("Error: Could not parse string, %w", err)
+		}
+		limit = int32(l)
+	} else if len(cmd.Args) != 0 {
+		return fmt.Errorf("usage: %s <limit> (optional)", cmd.Name)
+	}
+
+	posts, err := s.db.GetPostsForUser(context.Background(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  limit,
+	})
+	if err != nil {
+		return fmt.Errorf("Error: Could not get posts of user, %w", err)
+	}
+
+	for _, post := range posts {
+		fmt.Printf("%s from %s\n", post.PublishedAt.Time.Format("Mon 1 Jan"), post.FeedID)
+		fmt.Printf("--- %s ---\n", post.Title)
+		fmt.Printf("    %v\n", post.Description.String)
+		fmt.Printf("Link: %s\n", post.Url)
+		fmt.Println("=====================================")
+		fmt.Println()
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func main() {
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 	cmds.register("following", middlewareLoggedIn(handleListFeedFollows))
+	cmds.register("browse", middlewareLoggedIn(handleBrowse))
 
 	if len(os.Args) < 2 {
 		log.Fatalf("Error: Insufficient arguments")
